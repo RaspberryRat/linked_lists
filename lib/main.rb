9069186c61155836dbe5a0a62e54e2d2dacc981a
Graham <git@grahamirvine.com>
2022-06-18 15:23:40 -0600

    setup node class, still haven't added pointers. Added a number of methods to linklist class

--- lib/main.rb
+++ lib/main.rb
@@ -1,30 +1,43 @@
+require "pry-byebug"
+
 class LinkedList
+  attr_accessor :list
+  def initialize
+    @list = []
+  end
+
   def append(value)
+    list << Node.new.value(value) 
     # adds a new node containined value to end of list
   end
 
-  def preprend(value)
+  def prepend(value)
+    list.unshift(Node.new.value(value))
     # adds a new node containing a value to the start of the list
   end
 
   def size
+    list.length
     # returns total number of nodes in list
   end
 
   def head
     # returns node at position 0
+    list[0]
   end
 
   def tail
-    # returns node at position -1
+    list[-1]
   end
 
   def at(index)
     # returns node at index location
+    list[index]
   end
 
   def pop
-    # removes element element in the list
+    # removes last  element in the list
+    list.delete_at(-1)
   end
 
   def contains?(value)
@@ -53,14 +66,36 @@ class LinkedList
 end
 
 class Node
-  def value
-    return nil
-
+  def value(node_value = nil)
+    [node_value, next_node]
   end
 
-  def next_node
-    return nil
-
+  def next_node(pointer = nil)
+    pointer # should be to an index location right?
   end
-
 end
+
+l = LinkedList.new
+
+l.append(20)
+l.prepend(100)
+l.append(40)
+l.prepend(0)
+l.prepend(743)
+puts "size of list: #{l.size}"
+print "first node in list: #{l.head}\n"
+print "last node in list: #{l.tail}\n"
+print "node at poistion 3: #{l.at(3)}\n"
+l.pop
+
+
+
+
+
+
+
+
+
+
+
+print "full list: #{l.list}"
